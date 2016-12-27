bda5d485d6d71026af68766c9d37c44843aec600
sekhar karri <babusek@gmail.com>
2016-12-27 22:24:32 +0000

    Adding integers and values"

---
+++ src/com/numberconvertor/interfaces/Names.java
@@ -0,0 +1,16 @@
+package com.numberconvertor.interfaces;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by sekhar on 27/12/16.
+ */
+public interface Names {
+
+    Map<Integer,String> numberAndNameMap();
+
+    char digitsSeparator();
+
+    List<String> forms();
+}
